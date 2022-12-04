46d072cdc436e577e1c841619868a6db2b2415b1
Gopi <gopi@Gopis-MacBook-Pro.local>
2022-12-04 20:20:20 +0530

    image viewer test cases added

--- src/ImageViewer.test.js
+++ src/ImageViewer.test.js
@@ -17,22 +17,6 @@ describe("ImageViewer Component", () => {
     });
   });
 
-  describe('The image component', () => {
-    test('alt contains correct value', () => {
-      render(<RenderApiData />);
-      const testImage = document.querySelector("img");
-      expect(testImage.alt).toContain("The image alt tag for the large image");
-
-      test('src contains correct value', () => {
-        render(<ImageComponent size="large"/>)
-        const testImage = document.querySelector("img");
-        expect(testImage.alt).toContain("https://images.unsplash.com/photo-1657299143333-4a56a5519651?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzODYyODV8MXwxfGFsbHwxfHx8fHx8Mnx8MTY3MDE2MTQ2NA&ixlib=rb-4.0.3&q=80&w=1080");
-      })
-    });
-
-    
-  
-
   it("should render error message when api fails", async () => {
     api.RenderApiData.mockResolvedValue({});
     render(<RenderApiData />);
