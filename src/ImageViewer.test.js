b402488d22a9fb64537b9f493bd63bfd13d32e51
Gopi <gopi@Gopis-MacBook-Pro.local>
2022-12-04 20:01:22 +0530

    image viewer test cases added

--- src/ImageViewer.test.js
+++ src/ImageViewer.test.js
@@ -2,7 +2,6 @@ import { render, waitFor } from "@testing-library/react";
 import * as api from "./components/services/api";
 import RenderApiData from "./components/pages/displayApiTab";
 
-
 jest.mock();
 
 describe("ImageViewer Component", () => {
@@ -13,9 +12,26 @@ describe("ImageViewer Component", () => {
     });
     render(<RenderApiData />);
     await waitFor(() => {
+      // eslint-disable-next-line no-restricted-globals
       screen.getByText("Wasa Crispbread");
     });
   });
+
+  describe('The image component', () => {
+    test('alt contains correct value', () => {
+      render(<RenderApiData />);
+      const testImage = document.querySelector("img");
+      expect(testImage.alt).toContain("The image alt tag for the large image");
+
+      test('src contains correct value', () => {
+        render(<ImageComponent size="large"/>)
+        const testImage = document.querySelector("img");
+        expect(testImage.alt).toContain("https://images.unsplash.com/photo-1657299143333-4a56a5519651?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzODYyODV8MXwxfGFsbHwxfHx8fHx8Mnx8MTY3MDE2MTQ2NA&ixlib=rb-4.0.3&q=80&w=1080");
+      })
+    });
+
+    
+  
 
   it("should render error message when api fails", async () => {
     api.RenderApiData.mockResolvedValue({});
--- src/components/pages/displayApiTab/GridCard.js
+++ src/components/pages/displayApiTab/GridCard.js
@@ -6,7 +6,12 @@ const GridCard = ({ data, rowsPerColumn }) => {
         className="my-2 px-2 d-inline-block"
         style={{ width: widthValue + "%" }}
       >
-        <img src={data.urls.regular} alt="" className="w-100" height={150} />
+        <img
+          src={data.urls.regular}
+          className="w-100"
+          height={150}
+          alt={data.urls.regular}
+        />
         <p className="fs-6 text-truncate fw-semibold text-capitalize">
           {data.user.first_name}
         </p>
